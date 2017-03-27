aaebf894dfa57e8dbfc690b7c89dd09aecdd0f2b
Unknown <maksimmerzh@gmail.com>
2017-03-27 21:43:40 +0300

    fixed focus, refactoring

--- main.py
+++ main.py
@@ -4,39 +4,38 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
 from translate import Translator
 
-builder = gtk.Builder()
-builder.add_from_file("mainwindow.glade")
-
 
 class Handler:
     def __init__(self):
-        self.on_entry_grab_focus()
         return
 
-    def on_entry_key_press_event(self, *args):
-        if args[1].hardware_keycode == 36:
-            input_text = entry.get_text()
-            # translate
-            tr = Translator()
-            buffer1.set_text(tr.translate_google(input_text) + '\n\n\n\nGoogle Translate')
-            buffer2.set_text(tr.translate_yandex(input_text) + '\n\n\n\nYandex Translate')
-            buffer3.set_text(tr.dictionary_yandex(input_text) + '\n\n\n\nYandex Dictionary')
+    def on_entry_enter_press_event(self, *args): #TODO: рассмотреть вариант поиска по Ctrl + space
+        input_text = entry.get_text()
+        # entry.set_text(str(switcher.get_active()))
+        # translate
+        #TODO: вывод перевода по мере загрузки
+        tr = Translator()
+        buffer1.set_text(tr.translate_google(input_text) + '\n\n\n\nGoogle Translate')
+        buffer2.set_text(tr.translate_yandex(input_text) + '\n\n\n\nYandex Translate')
+        buffer3.set_text(tr.dictionary_yandex(input_text) + '\n\n\n\nYandex Dictionary')
 
     def onWindowDestroy(self, *args):
         gtk.main_quit()
 
-    #TODO: focus
-    def on_entry_grab_focus(self, *args):
-        return
-
     #TODO: switcher
 
-builder.connect_signals(Handler())
+builder = gtk.Builder()
+builder.add_from_file("mainwindow.glade")
+
 window = builder.get_object('window')
+window.set_title('Desktop translator')
 entry = builder.get_object('entry')
-textview1 = builder.get_object('textview4')
-textview2 = builder.get_object('textview5')
-textview3 = builder.get_object('textview6')
+switcher = builder.get_object('switch')
+textview1 = builder.get_object('textview1')
+textview2 = builder.get_object('textview2')
+textview3 = builder.get_object('textview3')
+
+builder.connect_signals(Handler())
 
 buffer1 = gtk.TextBuffer()
 buffer2 = gtk.TextBuffer()
